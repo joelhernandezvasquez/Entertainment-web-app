bcaf63d21b03008554d0143fcc1d569d42f40df7
joelhernandezvasquez <jhernandez98@outlook.es>
2022-03-29 13:40:02 -0400

    make the page fully responsive

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import '../../styles/main.scss';
+import Logo from '../../assets/logo.svg';
 
 const Login = () => {
+
+
+
   return (
-    <div>Logindfsdfsdf</div>
+    <section className='login_wrapper flex flex_column_center animate__animated animate__fadeIn' role="region">
+       <header>
+         <img src={Logo} alt="Red Icon of Movie" />
+       </header>
+
+       <main role="main">
+         <div className='login_container' role="form" aria-labelledby='sign-in-form'>
+           <h1 className='white_text fw_300 fs_800'>Login</h1>
+           <button className='signInBtn flex flex_center' aria-describedby='sign in google'> <i className="fa fa-google google_icon" aria-hidden="true"></i> Sign In with Google</button>
+           <p className='white_text fw_300 fs_300 text_center'>Don't have an account? <a className='google_link capitalize' href='https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp' target="_blank">sign up</a></p>
+         </div>
+       </main>
+    </section>
   )
 }
 
