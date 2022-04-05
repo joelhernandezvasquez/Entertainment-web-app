0bcf8ebd411d8c9a69f67fd40c5f3cb241a21401
joelhernandezvasquez <jhernandez98@outlook.es>
2022-04-05 15:10:07 -0400

    added accesibility

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -16,21 +16,21 @@ const Login = () => {
    }
 
   return (
-    <section className='login_wrapper flex flex_column_center animate__animated animate__fadeIn' role="region">
+    <section className='login_wrapper flex flex_column_center animate__animated animate__fadeIn'>
        <header>
-         <img src={Logo} alt="Movie Red Icon" role="img"/>
+         <img src={Logo} alt="Red Movie Logo" role="img"/>
        </header>
 
-       <main role="main">
+       <main>
          <div className='login_container' role="form" aria-labelledby='sign-in-form'>
            <h1 className='white_text fw_300 fs_800'>Login</h1>
            <button 
               onClick={()=> handlerLogin()}
              className='signInBtn flex flex_center'
               aria-describedby='sign in google'> 
-              <i className="fa fa-google google_icon" aria-hidden="true"></i> Sign In with Google
+              <i className="fa fa-google google_icon" role="img" aria-label='Google Icon' aria-hidden="true"></i> Sign In with Google
               </button>
-           <p className='white_text fw_300 fs_300 text_center'>Don't have an account? <a className='google_link capitalize' href='https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp' target="_blank">sign up</a></p>
+           <p className='white_text fw_300 fs_300 text_center'>Don't have an account? <a className='google_link capitalize' href='https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp' target="blank">sign up</a></p>
          </div>
        </main>
     </section>
